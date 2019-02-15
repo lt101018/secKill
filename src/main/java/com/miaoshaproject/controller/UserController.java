63aa5aa7da7d4668b8dcc90b2ab19352603121af
Teng Li <tengli@vt.edu>
2019-02-15 17:15:33 -0500

    implement get OTP message function and corresponding html file

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -15,13 +15,42 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @Controller("user")
 @RequestMapping("/user")
+@CrossOrigin //To avoid CORS error,because html file and localhost have different origin.
 public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+    //it's a proxy. thread local map is used to make different thread operating different requests.
+
+
+    @RequestMapping(value="/getotp", method={RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name="telephone")String telephone){
+        //generating otp code
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //store the relation of telephone numebr and otp code in HttpSession
+        httpServletRequest.getSession().setAttribute(telephone, otpCode);
+
+        //Send the OTP Code to user
+        //This is just for testing. OTP code should not print for real case.
+        System.out.println("******Testing: telephone:"+telephone+"&OtpCode:"+otpCode);
+
+        return CommonReturnType.create(null);
+    }
+
+
+
 
     @RequestMapping("/get")
     @ResponseBody
