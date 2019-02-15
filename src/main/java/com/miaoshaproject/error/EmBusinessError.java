c2851646bf34a6a76a0c87a50e9bda700bb2cc80
Teng Li <tengli@vt.edu>
2019-02-15 15:19:34 -0500

    Implement Exception Handler functionality

---
+++ src/main/java/com/miaoshaproject/error/EmBusinessError.java
@@ -0,0 +1,32 @@
+package com.miaoshaproject.error;
+
+public enum EmBusinessError implements CommonError {
+    PARAMETER_VALIDATION_ERROR(10001, "Invalid Input Value"),
+    UNKNOWN_ERROR(10002, "Unknown Error"),
+    USER_NOT_EXIST(20001,"User Not Exists"),
+    ;
+
+    private int errCode;
+    private String errMsg;
+
+    private EmBusinessError(int errCode, String errMsg){
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    @Override
+    public int getErrCode() {
+        return this.errCode;
+    }
+
+    @Override
+    public String getErrMsg() {
+        return this.errMsg;
+    }
+
+    @Override
+    public CommonError setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+        return this;
+    }
+}
